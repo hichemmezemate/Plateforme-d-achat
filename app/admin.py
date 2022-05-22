815c5a6c618f5fca58bf8488d5a3fe1882040aa9
Hichem Mezemate <hichem.mezemate@gmail.com>
2022-05-22 14:19:05 +0200

    cart item model

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from app.models import Product
+from app.models import Product, Cart, Item
 
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(Cart)
+admin.site.register(Item)
--- app/models.py
+++ app/models.py
@@ -14,3 +14,21 @@ class Product(models.Model):
 
     def available_inventory(self):
         return self.max_inventory - self.ordered_inventory
+
+
+class Cart(models.Model):
+    id = models.IntegerField(primary_key=True)
+    checked_out = models.BooleanField(default=False)
+
+    def total_ttc(self):
+        items = Item.objects.filter(cart_id=self.id)
+        total_ttc = 0
+        for item in items:
+            total_ttc = total_ttc + item.product.price_ttc()
+        return total_ttc
+
+
+class Item(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
